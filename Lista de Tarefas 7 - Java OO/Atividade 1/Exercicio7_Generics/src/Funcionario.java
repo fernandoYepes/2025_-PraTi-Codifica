c0b7f8217c459f866015e5e4117dd951ae591342
fernandoYepes <122755418+fernandoYepes@users.noreply.github.com>
2025-11-15 18:14:56 -0300

    Delete Lista de Tarefas 7 - Java OO directory

--- Lista de Tarefas 7 - Java OO/Atividade 1/Exercicio7_Generics/src/Funcionario.java
+++
@@ -1,9 +0,0 @@
-// Entidade Funcionario (usando record).
-// Implementa Identificavel com um ID do tipo String (CPF).
-public record Funcionario(String cpf, String nome) implements Identificavel<String> {
-
-    @Override
-    public String getId() {
-        return this.cpf;
-    }
-}
