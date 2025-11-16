510ed4940c8d180aadd5caa996d89ef3e45fda2c
fernandoYepes <122755418+fernandoYepes@users.noreply.github.com>
2025-11-15 22:39:21 -0300

    Delete Entrega_Java_POO_FernandoYepes directory

--- Entrega_Java_POO_FernandoYepes/Exercicio7_Generics/src/EntidadeNaoEncontradaException.java
+++
@@ -1,8 +0,0 @@
-// Exceção (unchecked) lançada quando uma entidade não é
-// encontrada no repositório.
-public class EntidadeNaoEncontradaException extends RuntimeException {
-
-    public EntidadeNaoEncontradaException(String mensagem) {
-        super(mensagem);
-    }
-}
--- Entrega_Java_POO_FernandoYepes/Exercicio8_Strategy/src/CepInvalidoException.java
+++
@@ -1,8 +0,0 @@
- // Exceção "checked" para CEPs que não passam na validação.
-
-public class CepInvalidoException extends Exception {
-
-    public CepInvalidoException(String mensagem) {
-        super(mensagem);
-    }
-}
